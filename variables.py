017191c241c994ebf51e3a6f681b620fa64a9578
WallaceNMe <wallacenme279@gmail.com>
2026-02-09 08:37:35 -0500

    Added variables file referenced in 3DA
    Initialized lists for various dragon types, traditional mortals, and names for different fantasy races.

---
+++ variables.py
@@ -0,0 +1,79 @@
+black_dragons_list = ["Black 1", "Black 2", "Black 3", "Black 5", "Black 6", "Black 7", "Black 9"]
+blue_dragons_list = ["Blue 1", "Blue 2", "Blue 4", "Blue 6", "Blue 7", "Blue 9", "Blue 11"]
+brass_dragons_list = ["Brass 1", "Brass 2", "Brass 3", "Brass 4", "Brass 5", "Brass 7", "Brass 9"]
+bronze_dragons_list = ["Bronze 1", "Bronze 3", "Bronze 6", "Bronze 7", "Bronze 8", "Bronze 9", "Bronze 11"]
+copper_dragons_list = ["Copper 1", "Copper 3", "Copper 5", "Copper 6", "Copper 7", "Copper 8", "Copper 10"]
+gold_dragons_list = ["Gold 2", "Gold 4", "Gold 6", "Gold 8", "Gold 9", "Gold 11", "Gold 13"]
+green_dragons_list = ["Green 1", "Green 2", "Green 4", "Green 5", "Green 6", "Green 8", "Green 10"]
+red_dragons_list = ["Red 2", "Red 3", "Red 5", "Red 7", "Red 8", "Red 10", "Red 12"]
+silver_dragons_list = ["Silver 2", "Silver 3", "Silver 6", "Silver 7", "Silver 8", "Silver 10", "Silver 12"]
+white_dragons_list = ["White 1", "White 2", "White 3", "White 4", "White 5", "White 6", "White 8"]
+
+traditional_mortals_list = ["The Archmage 8", "Bahamut 13", "Dracolich 10", "The Dragonslayer 8", "The Druid 6", "The Fool 3", "The Priest 5", "The Princess 4", "The Thief 7", "Tiamat 13"]
+
+deck_list = ["Black 1", "Black 2", "Black 3", "Black 5", "Black 6", "Black 7", "Black 9", "Blue 1", "Blue 2", "Blue 4", "Blue 6", "Blue 7", "Blue 9", "Blue 11", "Brass 1", "Brass 2", "Brass 3", "Brass 4", "Brass 5", "Brass 7", "Brass 9", "Bronze 1", "Bronze 3", "Bronze 6", "Bronze 7", "Bronze 8", "Bronze 9", "Bronze 11", "Copper 1", "Copper 3", "Copper 5", "Copper 6", "Copper 7", "Copper 8", "Copper 10", "Gold 2", "Gold 4", "Gold 6", "Gold 8", "Gold 9", "Gold 11", "Gold 13", "Green 1", "Green 2", "Green 4", "Green 5", "Green 6", "Green 8", "Green 10", "Red 2", "Red 3", "Red 5", "Red 7", "Red 8", "Red 10", "Red 12", "Silver 2", "Silver 3", "Silver 6", "Silver 7", "Silver 8", "Silver 10", "Silver 12", "White 1", "White 2", "White 3", "White 4", "White 5", "White 6", "White 8", "The Archmage 8", "Bahamut 13", "Dracolich 10", "The Dragonslayer 8", "The Druid 6", "The Fool 3", "The Priest 5", "The Princess 4", "The Thief 7", "Tiamat 13"]
+
+name_list = [
+    # Human names
+    "Aldric Thornheart",
+    "Mira Blackwood",
+    "Gareth Ironfoot",
+    "Elara Moonwhisper",
+    "Thorne Greystone",
+    
+    # Elf names
+    "Faelwen Silverleaf",
+    "Thalion Starweaver",
+    "Lysandra Dawnbringer",
+    "Aelar Windwhisper",
+    "Silaqui Nightshade",
+    
+    # Dwarf names
+    "Thorin Ironbeard",
+    "Brenna Stoneforge",
+    "Balin Hammerfall",
+    "Gilda Deepdelver",
+    "Kromm Firestone",
+    
+    # Halfling names
+    "Pippin Goodbarrel",
+    "Rosie Underhill",
+    "Merric Tosscobble",
+    "Lidda Tealeaf",
+    "Finnegan Brushgather",
+    
+    # Dragonborn names
+    "Kriv Thunderscale",
+    "Sora Flameheart",
+    "Balasar Ironclaw",
+    "Mishann Emberwing",
+    "Torinn Goldbreath",
+    
+    # Tiefling names
+    "Zariel Nightshade",
+    "Virtue Ashenblade",
+    "Morthos Shadowhorn",
+    "Akta Hellfire",
+    "Nemeia Darkwhisper",
+    
+    # Half-Orc names
+    "Grommash Skullcrusher",
+    "Yevelda Ironhide",
+    "Thokk Bloodfang",
+    "Krazka Bonegrinder",
+    "Gorzak Steelfist",
+    
+    # Gnome names
+    "Bimpnoddle Tinkertop",
+    "Zanna Sparklegem",
+    "Fibblestib Cogsworth",
+    "Nyx Quickfingers",
+    "Jebeddo Moonwhirl",
+    
+    # Half-Elf names
+    "Aramil Silvermoon",
+    "Liara Softwind",
+    "Talos Brightwood",
+    "Shiera Ravenshadow",
+    "Eryndor Stormborn"
+]
