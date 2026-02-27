e557363521ebf60322834db836f8fcff1a76c971
WallaceNMe <wallacenme279@gmail.com>
2026-02-27 08:30:49 -0500

    Improve player turn and ante card selection
    Finished the ante phase with basic functionality for computers and card selection for players. Improved readability with print_board() function and added a colored tracker to mark the current round leader.

--- ThreeDragonAnte.py
+++ ThreeDragonAnte.py
@@ -4,7 +4,8 @@ from collections import Counter
 clear = lambda: os.system('clear')
 
 
-# Modify code to initialize user class as player 1. Build functionality to display their hand and allow choice of ante card
+# Begin working on player main turn
+# track next_round_leader for highest played str
 
 
 GREEN_BG = '\033[42m\033[30m\033[1m'
@@ -63,18 +64,18 @@ class User():
     return self.name
 
   def draw(self, num):
+    print(f"{self.name} draws {num} card(s)")
+    # if num == 1:
+    #   print(BLUE_BG_WHITE + f"{self.name} draws {num} card" + RESET)
+    # elif num > 1:
+    #   print(BLUE_BG_WHITE + f"{self.name}  draws {num} cards" + RESET)
+    # else:
+    #   print(RED_BG + "CARD NUM PRINT ERROR" + RESET)
     for i in range(num):
       new_card = draw_pile.pop()
       self.hand.append(new_card)
       check_reshuffle()
-    if num == 1:
-      print(YELLOW_BG + f"{self.name} draws {num} card" + RESET)
-    elif num > 1:
-      print(YELLOW_BG + f"{self.name}  draws {num} cards" + RESET)
-    else:
-      print(RED_BG + "CARD NUM PRINT ERROR" + RESET)
-
-    print(f"You have drawn: {new_card}")
+      print(f"You have drawn: {new_card[0]}{RESET}")    
 
   def receive_gold(self, amount):
     self.gold += amount
@@ -84,19 +85,61 @@ class User():
     if to_player:
       to_player.receive_gold(amount)
 
-  def ante_card(self):
-    #Ante lowest str card
-    self.card_to_ante = self.hand[0]
-    for i in range(len(self.hand)):
-      if self.hand[i][1] < self.card_to_ante[1]:
-        self.card_to_ante = self.hand[i]
+  def print_status(self):
+      if self == round_leader:
+        print(f"{self} {CYAN_BG}(RL){RESET}: {self.gold}GP, {self.hand_size} Cards")
+      else:
+        print(f"{self}: {self.gold}GP, {self.hand_size} Cards")
+      # reformat flight for printing
+      flight_reformatted = [value[0] for value in self.flight]
+      print(f"  Flight: {', '.join(flight_reformatted)}")
 
-    # Remove from player hand and return card and the player it came from
-    self.hand.remove(self.card_to_ante)
-    return [self, self.card_to_ante]
+  def ante_card(self):
+    print("YOUR HAND:")
+    for i, card in enumerate(self.hand, 1):
+      print(f"  {i}. {card[0]}")
+    
+    card_index = None
+    while card_index not in range(1, self.hand_size + 1):
+      try:
+        card_index = int(input(f"Enter the card number you want to ante (1-{self.hand_size}): "))
+      except ValueError:
+        print("Please enter a valid number.")
+        card_index = None
+    
+    card_to_ante = self.hand[card_index - 1]
+    self.hand.remove(card_to_ante)
+    return [self, card_to_ante]
+    
+    # #Ante lowest str card
+    # self.card_to_ante = self.hand[0]
+    # for i in range(len(self.hand)):
+    #   if self.hand[i][1] < self.card_to_ante[1]:
+    #     self.card_to_ante = self.hand[i]
+
+    # # Remove from player hand and return card and the player it came from
+    # self.hand.remove(self.card_to_ante)
   
-  def main_turn(self):
-    pass
+  def main_turn(self,last_str_played=None):
+    print(f"\n--------------- PLAYER TURN ---------------")
+    # Print Hand
+    print("YOUR HAND:")
+    for i, card in enumerate(self.hand, 1):
+      print(f"  {i}. {card[0]}")
+    # Demand card to play
+    card_index = None
+    while card_index not in range(1, self.hand_size + 1):
+      try:
+        if last_str_played:
+          print(f"The last card played was STR {last_str_played}")
+        card_index = int(input(f"Enter the card number you want to play (1-{self.hand_size}): "))
+      except ValueError:
+        print("Please enter a valid number.")
+        card_index = None
+
+    card_to_play = self.hand[card_index - 1]
+    self.flight.append(card_to_play)
+    self.hand.remove(card_to_play)
 
 class Player():
 
@@ -120,16 +163,17 @@ class Player():
     return self.name
 
   def draw(self, num):
+    print(f"{self.name} draws {num} card(s)")
     for i in range(num):
       new_card = draw_pile.pop()
       self.hand.append(new_card)
       check_reshuffle()
-    if num == 1:
-      print(YELLOW_BG + f"{self.name} draws {num} card" + RESET)
-    elif num > 1:
-      print(YELLOW_BG + f"{self.name}  draws {num} cards" + YELLOW_BG)
-    else:
-      print(RED_BG + "CARD NUM PRINT ERROR" + RESEt)
+    # if num == 1:
+    #   print(BLUE_BG_WHITE + f"{self.name} draws {num} card" + RESET)
+    # elif num > 1:
+    #   print(BLUE_BG_WHITE + f"{self.name}  draws {num} cards" + RESET)
+    # else:
+    #   print(RED_BG + "CARD NUM PRINT ERROR" + RESEt)
 
   def receive_gold(self, amount):
     self.gold += amount
@@ -139,6 +183,15 @@ class Player():
     if to_player:
       print(f"{self.name} pays {amount} gold to {to_player}.")
       to_player.receive_gold(amount)
+
+  def print_status(self):
+      if self == round_leader:
+        print(f"{self} {CYAN_BG}(RL){RESET}: {self.gold}GP, {self.hand_size} Cards")
+      else:
+        print(f"{self}: {self.gold}GP, {self.hand_size} Cards")
+      # reformat flight for printing
+      flight_reformatted = [value[0] for value in self.flight]
+      print(f"  Flight: {', '.join(flight_reformatted)}")
 
   def ante_card(self):
     #Ante lowest str card
@@ -152,7 +205,18 @@ class Player():
     return [self, self.card_to_ante]
   
   def main_turn(self):
-    pass
+    # determine_strategy() function
+
+    # plays strongest cards
+    # Determine strongest
+    self.strongest_card = self.hand[0]
+    for item in self.hand:
+      if item[1] > self.strongest_card[1]:
+        self.strongest_card = item
+    # Add to flight, remove from hand
+    self.flight.append(strongest_card)
+    self.hand.remove(strongest_card)
+
     
 # --------------------------------------
 # --------------------------------------
@@ -176,8 +240,18 @@ def check_reshuffle():
     discard_pile = []
     shuffle_deck()
 
+def print_board():
+  global stakes, round_leader, ante_pile
+  print("\n--------------- TABLE VIEW ---------------")
+  print(f"Stakes: {stakes}")
+  reformatted_ante = [value[0] for value in ante_pile]
+  print(f"Ante: {", ".join(reformatted_ante)}")
+  for player in player_list:
+    player.print_status()
+  #print("------------------------------")
+
 def ante_phase():
-  global stakes, current_round_leader
+  global stakes, round_leader, ante_pile
   
   acceptable_ante = False
   while not acceptable_ante:
@@ -188,19 +262,19 @@ def ante_phase():
       returned_list.append(player_list[i].ante_card())
     
     # Print ante results
+    print("\n--------------- ANTE PHASE ---------------")
+    # Print Player Antes
     for i in range(len(returned_list)):
       print(f"{returned_list[i][0]} antes: {returned_list[i][1][0]}")
-    #print(returned_list)
-  
-    # ----- Check Edge Case -----
+
+    # ----- Check All Tied (Re-ante) -----
 
     # Get all strength values
     strength_values = [return_value[1][1] for return_value in returned_list]
     
     # Check if all cards have the same strength (all tied)
     if len(set(strength_values)) == 1:
-      print("\nAll ante cards were tied, Each player Draws a card and re-antes.\n")
-      time.sleep(1)
+      print("\n**All ante cards were tied, Each player draws a card and re-antes.**\n")
       for return_value in returned_list:
         # Discard ante cards
         discard_pile.append(return_value[1])
@@ -212,6 +286,7 @@ def ante_phase():
   
   # ----- Resume Ante -----
 
+  # Add cards to ante_pile
   ante_pile = [return_value[1] for return_value in returned_list]
 
   # Find the highest strength card
@@ -238,23 +313,32 @@ def ante_phase():
     # Find the player who played that card
     first_player = [r[0] for r in returned_list if r[1][1] == highest_untied][0]
   else:
-    # All cards are tied - use previous round's leader or default
-    if current_round_leader:
-        first_player = current_round_leader
+    # All cards are tied with at least one other card - use previous round's leader or default
+    if round_leader:
+        first_player = round_leader
+        print("The previous round leader will start the next gambit.")
     else:
         # First round and all tied - random selection
-        print("All cards are tied, so the first player will be random.")
+        print("All cards are tied with at least one other, so the first player will be random.")
         first_player = random.choice(player_list)
         print(f"The first player will be {first_player}")
     
   
-  return first_player
+  return first_player, highest_str_value
 
 def start_gambit():
-  current_round_leader = ante_phase()
-  print(f"\n{current_round_leader} will start the round.\n")
-
+  global round_leader, gambit_number
+  gambit_number += 1
+  print(f"BEGIN GAMBIT {gambit_number}")
+  print("--------------- PLAYER ANTE ---------------")
   # ANTE PHASE RETURNS A PLAYER CLASS
+  round_leader, ante_gold = ante_phase()
+  print(f"\nEach player antes {ante_gold} gold.\n{round_leader} will start the round.")
+  time.sleep(2)
+  print_board()
+
+  highest_str_card_player = None
+  # Each player 
 
 # --------------------------------------
 # --------------------------------------
@@ -275,8 +359,8 @@ player_names_list = [user_name]
 num = 1
 for i in range(player_count):
   num += 1
-  player_names_list.append(f"Player {num}")
-# FOR ACTUAL NAMES:
+  player_names_list.append(f"Computer {num}")
+# ----- FOR ACTUAL NAMES -----
 # user_name = input("Enter your character's name: ")
 # if user_name == "":
 #   user_name = "TEST_USER"
@@ -288,7 +372,7 @@ for i in range(player_count):
 #   name_list.remove(name)
 #   player_names_list.append(name)
 
-# ---------- Starting Hand ----------
+# ---------- Starting Hands ----------
 shuffle_deck()
 hand_lists = []
 for i in range(player_count):
@@ -302,27 +386,28 @@ for i in range(player_count):
 # ---------- Initialize Classes ----------
 starting_gold = 10 * player_count
 player_list = []
-for i in range(player_count):
-  player_list.append(Player(i, player_names_list[i], hand_lists[i], starting_gold))
+
+# USER CLASS
+player_list.append(User(0, player_names_list[0], hand_lists[0], starting_gold))
+my_var_ref[f"User's Hand"] = hand_lists[0]
+
+# Computer classes
+for i in range(player_count - 1):
+  player_list.append(Player(i+1, player_names_list[i+1], hand_lists[i+1], starting_gold))
   my_var_ref[f"Player {i}'s hand"] = hand_lists[i]
 
-#------------------------------------------
+# ---------- Variable Reference ----------
 my_var_ref["player_count"] = player_count
 my_var_ref["user_name"] = user_name
 my_var_ref["player_names_list"] = player_names_list
 my_var_ref["starting gold"] = starting_gold
 my_var_ref["use_pile"] = use_pile
 my_var_ref["hand_lists (starting)"] = hand_lists
-#print_ref()
 
-# --------------------------------------
-# --------------------------------------
-# --------------------------------------
-
-# Start Game
+# ---------- Additional Variables ----------
 stakes = 0
-current_round_leader = None
+round_leader = None
+gambit_number = 0
 
+# ---------- Start Game ----------
 start_gambit()
-
-
